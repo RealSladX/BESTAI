18fe47bedbd19eced9c9d0818912b2be6a9c4ff6
Dalston Karto <142933105+RealSladX@users.noreply.github.com>
2024-04-16 22:06:07 -0700

    Update yoloASL.py

--- ASL AI/yoloASL.py
+++ ASL AI/yoloASL.py
@@ -2,4 +2,8 @@ from ultralytics import YOLO
 
 model = YOLO('ASLAlphabet.pt')
 
-results = model(source=0, show=True, conf = 0.3, save=True)
+results = model(source=0, show=True, conf = 0.3, save=True, stream=True)
+for r in results:
+    boxes = r.boxes  # Boxes object for bbox outputs
+    masks = r.masks  # Masks object for segment masks outputs
+    probs = r.probs  # Class probabilities for classification outputs
